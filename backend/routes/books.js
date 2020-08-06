460a588b72add0e3e445b4198117a56291035d96
fahrieramadhan234 <57474351+fahrieramadhan234@users.noreply.github.com>
2020-08-07 00:30:52 +0700

    update mysql

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Books = require("../models/db");
+const Books = require("../models/dbBooks");
 
 //GET ALL BOOKS
 router.get("/", async (req, res, next) => {
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
+const mysql = require("mysql");
 const books = require("./routes/books");
+const user = require("./routes/user");
 
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "node_js",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connect to Mysql Success");
+});
 const port = process.env.PORT || 5000;
 const uri = "mongodb://localhost:5000:27017/books";
 
@@ -16,6 +29,7 @@ const connection = mongoose.connection;
 
 app.use(bodyParser.json());
 app.use("/book", books);
+app.use("/user", user);
 
 connection.once("open", () => {
   console.log("Connect to MongoDB");
